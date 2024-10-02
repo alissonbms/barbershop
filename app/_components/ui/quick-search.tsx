879c384620ec7f9e0d3e130261b03ed9215aab76
alissonbms <alissonnewdev@gmail.com>
2024-10-02 00:41:47 -0300

    feat: improve QuickSearch component, adding more options

--- app/_components/ui/quick-search.tsx
+++ app/_components/ui/quick-search.tsx
@@ -1,59 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "./button";
+import { quickSearchOptions } from "@/app/_constants/search";
 
 const QuickSearch = () => {
   return (
     <div className="my-6 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-      <Button className="flex min-w-[125px] gap-2" variant="secondary">
-        <Image
-          src="/cabelo.svg"
-          alt="procurar serviços para o cabelo "
-          width={16}
-          height={16}
-        />
-        Cabelo
-      </Button>
-
-      <Button className="flex min-w-[125px] gap-2" variant="secondary">
-        <Image
-          src="/barba.svg"
-          alt="procurar serviços para a barba "
-          width={16}
-          height={16}
-        />
-        Barba
-      </Button>
-
-      <Button className="flex min-w-[125px] gap-2" variant="secondary">
-        <Image
-          src="/acabamento.svg"
-          alt="procurar serviços de acabamento "
-          width={16}
-          height={16}
-        />
-        Acabamento
-      </Button>
-
-      <Button className="flex min-w-[125px] gap-2" variant="secondary">
-        <Image
-          src="/barba.svg"
-          alt="procurar serviços para a barba "
-          width={16}
-          height={16}
-        />
-        Pezinho
-      </Button>
-
-      <Button className="flex min-w-[125px] gap-2" variant="secondary">
-        <Image
-          src="/acabamento.svg"
-          alt="procurar serviços de acabamento "
-          width={16}
-          height={16}
-        />
-        Sobrancelha
-      </Button>
+      {quickSearchOptions.map((quickSearchOption) => (
+        <Button
+          key={quickSearchOption.title}
+          variant="secondary"
+          className="flex min-w-[120px] items-center gap-2"
+        >
+          <Image
+            src={quickSearchOption.imageUrl}
+            alt={quickSearchOption.title}
+            width={16}
+            height={16}
+          />
+          {quickSearchOption.title}
+        </Button>
+      ))}
     </div>
   );
 };
---
+++ app/_constants/search.ts
@@ -0,0 +1,36 @@
+interface quickSearchOptionsProps {
+  title: string;
+  imageUrl: string;
+}
+
+export const quickSearchOptions: quickSearchOptionsProps[] = [
+  {
+    title: "Cabelo",
+    imageUrl: "/cabelo.svg",
+  },
+
+  {
+    title: "Barba",
+    imageUrl: "/barba.svg",
+  },
+
+  {
+    title: "Acabamento",
+    imageUrl: "/acabamento.svg",
+  },
+
+  {
+    title: "Sobrancelha",
+    imageUrl: "/sobrancelha.svg",
+  },
+
+  {
+    title: "Massagem",
+    imageUrl: "/massagem.svg",
+  },
+
+  {
+    title: "Hidratacão",
+    imageUrl: "/hidratacao.svg",
+  },
+];
